31e59b68c26a2d5b73c7f2b656800576b8dd2c5b
TristanB <bonnal.tristan@hotmail.fr>
2024-07-21 22:13:29 +0200

    init symfony components + vue + tailwind

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+import '../styles/app.css';
+import { createApp } from 'vue'
+import App from '../vue/App.vue'
+
+createApp(App).mount('#app')
---
+++ assets/vue/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {computed, ref} from 'vue';
+const test = ref('hey');
+</script>
+
+<template>
+    <p>init vue ok {{test}}</p>
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div>
+            <div class="text-xl font-medium text-black">ChitChat</div>
+            <p class="text-slate-500">You have a new message!</p>
+        </div>
+    </div>
+</template>
